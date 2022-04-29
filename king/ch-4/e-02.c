68849da550f671fedbc8251d14c898db71d9dbdd
nitinsavant <me@nitinsavant.com>
2022-04-28 17:00:52 -0700

    chapter 4

---
+++ king/ch-4/e-02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+
+    printf("%d\n", (-i) / j);
+
+    printf("%d\n", - (i / j));
+
+    // unary +/- has higher precedence than '/',
+    // so '-i / j' = '(-i) / j',
+    // but the 2nd expression (above) uses parentheses to elevate the precedence of '/',
+    // and since division is implementation-defined behavior in C89,
+    // we can't guarantee that the expressions always evaluate to the same value.
+    // In the C99 standard, however, it would be the same,
+    // because division is always trunated to 0
+}
