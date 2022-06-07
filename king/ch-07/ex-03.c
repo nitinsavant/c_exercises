bc3c02978c049d75bcaf29af6d82899fb5293ff8
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2022-06-07 08:22:14 -0500

    Ch 7 stuff

---
+++ king/ch-07/ex-03.c
@@ -0,0 +1,6 @@
+/*
+ * (a) short unsigned int -- legal
+ * (b) short float -- illegal; there is no 'short float' type
+ * (c) long double -- legal; more precise than traditional double
+ * (d) unsigned long -- legal
+ */
---
+++ king/ch-07/ex-11.c
@@ -0,0 +1,5 @@
+/*
+ * int * float / double --> float / double --> double
+ *
+ * int converted to float and then float converted to double
+ */
